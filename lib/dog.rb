c0918e476e7ac692cf516ba981bd4b07ef06b78e
dami21 <dami.adenekan@schaumburgchristian.com>
2018-08-10 16:42:14 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,25 @@
 # dog.rb
 class Dog 
-  def fido=(fido)
-    @fido = fido 
-  end
-  def fido 
-    @fido
-  end 
-  def snoopy=(snoopy)
-    @snoopy = snoopy
-  end 
-  def snoopy 
-    @snoopy
-  end 
-  def lassie=(lassie)
-    @lassie = lassie
-  end 
-  def lassie 
-    @lassie 
-  end 
+  # def fido=(fido)
+  #   @fido = fido 
+  # end
+  # def fido 
+  #   @fido
+  # end 
+  # def snoopy=(snoopy)
+  #   @snoopy = snoopy
+  # end 
+  # def snoopy 
+  #   @snoopy
+  # end 
+  # def lassie=(lassie)
+  #   @lassie = lassie
+  # end 
+  # def lassie 
+  #   @lassie 
+  # end 
     
 end 
+
+fido = Dog.new 
+
